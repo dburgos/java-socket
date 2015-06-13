73c94a880c7f138c11aac7016eb15885918c8bee
dburgos <david.burgos.risco@gmail.com>
2015-06-13 11:02:45 +0200

    Get params supporting little endian format

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -1,17 +1,26 @@
 import java.io.*;
 import java.net.*;
+import java.nio.*;
 
 class TCPServer {
   public static void main(String argv[]) throws Exception
   {
     String clientSentence;
     String capitalizedSentence;
+    String objectId;
+    String temperature;
     ServerSocket TCPSocket = new ServerSocket(6789);
     while(true)
     {
       Socket connectionSocket = TCPSocket.accept();
       BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
       clientSentence = inFromClient.readLine();
+
+      ByteBuffer buffer = ByteBuffer.wrap(clientSentence.getBytes());
+      buffer.order(ByteOrder.LITTLE_ENDIAN);
+      objectId = new String(buffer.get(new byte[], 0, 16), Charset.forName("UTF-8"));
+      temperature = new String(buffer.get(new byte[], 17, 2), Charset.forName("UTF-8"));
+
     }
   }
 }
