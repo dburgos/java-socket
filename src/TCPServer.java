d7c270fa935deb0c68f5550692e0516c7eb1d0d5
dburgos <david.burgos.risco@gmail.com>
2015-06-13 15:29:30 +0200

    Add MongoDB support

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -1,6 +1,13 @@
 import java.io.*;
 import java.net.*;
 import java.nio.*;
+import java.util.Date;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
 
 class TCPServer {
   public static void main(String argv[]) throws Exception
@@ -20,6 +27,15 @@ class TCPServer {
       buffer.order(ByteOrder.LITTLE_ENDIAN);
       objectId = new String(buffer.get(new byte[], 0, 16), Charset.forName("UTF-8"));
       temperature = new String(buffer.get(new byte[], 17, 2), Charset.forName("UTF-8"));
+
+      MongoClient mongo = new MongoClient("localhost", 27017);
+      DB db = mongo.getDB("testdb");
+      DBCollection table = db.getCollection("status_log");
+      BasicDBObject document = new BasicDBObject();
+      document.put("device", objectId);
+      document.put("temperature", temperature);
+      document.put("saved_at", new Date());
+      table.insert(document);
 
     }
   }
